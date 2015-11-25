bd0736d1742efe9726a5176063126112ae3a4a41
Gorhn <mail.place.a@gmail.com>
2015-11-25 02:26:40 +0100

    Code final
    Commenté/revu

--- src/content/TextContent.java
+++ src/content/TextContent.java
@@ -1,17 +1,38 @@
 package content;
 
+/**
+ * Class représentant un contenu textuel.
+ * @author Place Leclercq
+ * 
+ */
+
 public class TextContent implements Content{
 
+/**
+ * Variable contenant le message de la lettre.
+ */
 	private String message;
 	
+/**
+ * Modifie le message de la lettre.
+ * @param le message qui doit être assigné à la lettre.
+ */
 	public TextContent(String message){
 		this.message = message;
 	}
 
+/**
+ * Renvoie le message contenu dans la lettre.
+ * @return le message contenu dans la lettre.
+ */
 	public String getMessage() {
 		return this.message;	
 	}
 	
+/**
+ * Renvoie la description du contenu de la lettre.
+ * @return la description du contenu de la lettre.
+ */
 	public String getDescription() {
 		return "";	
 	}
