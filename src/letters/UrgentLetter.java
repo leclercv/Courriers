b5e5d4dece3b02510c75eeabd3ff66fb55181668
Gorhn <mail.place.a@gmail.com>
2015-11-25 03:11:50 +0100

    Code final
    Commenté/revu

--- src/letters/UrgentLetter.java
+++ src/letters/UrgentLetter.java
@@ -2,21 +2,40 @@ package letters;
 
 import entity.Inhabitant;
 
+/**
+ * Class représentant une lettre à envoyer de façon urgente.
+ */
 public class UrgentLetter extends Letter<Letter<?>> {
 	
+	/**
+	 * Constructeur de base d'une lettre à envoyer de façon urgente.
+	 * @param sender L'habitant ayant envoyé la lettre.
+	 * @param receiver L'habitant destinataire de la lettre.
+	 * @param content La lettre à envoyer de façon urgente.
+	 */
 	public UrgentLetter(Inhabitant sender, Inhabitant receiver, Letter<?> content) {
 		super(sender, receiver, content);
 	}
 	
+	/**
+	 * Initialise le coût initial de la lettre.
+	 */
 	protected void initCost() {
 		cost = 2;
 	}
 
+	/**
+	 * Renvoie la description de la lettre.
+	 * @return la description de la lettre.
+	 */
 	@Override
 	public String getDescription() {
 		return "an urgent letter whose content is "+content.getDescription();
 	}
 
+	/**
+	 * Effectue l'action nécessaire à la réception de la lettre envoyée de façon urgente.
+	 */
 	@Override
 	public void doAction() {
 		content.doAction();
